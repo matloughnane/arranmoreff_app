5aa36548ade5eb58ad0ee6c118adb892af902d5e
Matthew Loughnane <mat.loughnane@gmail.com>
2014-12-24 23:52:27 +0000

    Materialise Design Completed

---
+++ beta/assets/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+	$('.datepicker').pickadate();
+	$('select').not('.disabled').material_select();
+	
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
